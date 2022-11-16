49f396d74282ae5c18e20d41a15a840582089f8e
0xdebaser <0xdebaser@gmail.com>
2022-11-15 16:30:42 -1000

    added back in 2022 Wobble Gobble

--- src/data/races.js
+++ src/data/races.js
@@ -1,11 +1,11 @@
 export const upcomingRaces = [
   // DO NOT DELETE THE FIRST ENTRY ("Please select...") OR MOVE IT!!!
   { name: "Please select a race" },
-  // {
-  //   name: "2022 Wobble Gobble",
-  //   distances: ["1 mile", "5k", "10k"],
-  //   password: "1234",
-  // },
+  {
+    name: "2022 Wobble Gobble",
+    distances: ["1 mile", "5k", "10k"],
+    password: "1234",
+  },
   {
     name: "Test Race 1",
     distances: ["4 mile", "5 mile", "6 mile"],
