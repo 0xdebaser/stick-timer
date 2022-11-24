602cd1613dd9bcafafed4982aac9c35d37dfd7ed
0xdebaser <0xdebaser@gmail.com>
2022-11-23 17:28:49 -1000

    bug fix

--- src/components/resultsTable/preliminaryResultsTable.component.jsx
+++ src/components/resultsTable/preliminaryResultsTable.component.jsx
@@ -4,14 +4,16 @@ import "./preliminaryResultsTable.styles.scss";
 import ExportButton from "./exportButton.component";
 
 function PreliminaryResultsTable(props) {
-  const [sortedRaceResults, setSortedRaceResults] = useState();
+  const [sortedRaceResults, setSortedRaceResults] = useState([]);
 
   async function sortByStickNumber(results) {
     const tempArray = [];
+    console.log(`results:`);
+    console.log(results);
     const numberOfSticks = results.length;
+    console.log(`number of sticks: ${numberOfSticks}`);
     for (let n = 1; n <= numberOfSticks; n++) {
       for (let i = 0; i < results.length; i++) {
-        console.log(results[i]);
         if (results[i] && results[i].stickNumber === n) {
           tempArray.push(results[i]);
         }
@@ -20,8 +22,24 @@ function PreliminaryResultsTable(props) {
     setSortedRaceResults(tempArray);
   }
 
+  function createResultsArray(resultsObject) {
+    const keysArray = Object.keys(resultsObject);
+    console.log(keysArray);
+    const resultsArray = [];
+    if (keysArray.length > 0) {
+      for (let i = 0; i <= keysArray.length; i++) {
+        if (resultsObject.hasOwnProperty(i.toString())) {
+          resultsArray.push(resultsObject[i.toString()]);
+        }
+      }
+    }
+    return resultsArray;
+  }
+
   useEffect(() => {
-    sortByStickNumber(props.currentRace.finishers);
+    console.log("useEffect running");
+    const resultsArray = createResultsArray(props.currentRace.finishers);
+    sortByStickNumber(resultsArray);
   }, [props.currentRace.finishers]);
 
   if (sortedRaceResults) {
