02da352d611bfddc06e828852e2c35a95487719d
0xdebaser <0xdebaser@gmail.com>
2023-11-30 08:30:41 -1000

    finalized 2023 Wobble Gobble

--- src/data/races.js
+++ src/data/races.js
@@ -2,11 +2,6 @@ export const upcomingRaces = [
   // DO NOT DELETE THE FIRST ENTRY ("Please select...") OR MOVE IT!!!
   { name: "Please select a race" },
   {
-    name: "2023_Wobble_Gobble",
-    distances: ["1 miles", "3.5 miles", "6.4 miles"],
-    password: "1234",
-  },
-  {
     name: "Test Race 1",
     distances: ["4 mile", "5 mile", "6 mile"],
     password: "1234",
