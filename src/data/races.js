4037f27d0a40d3eabb0cb88a7fba824e45c314f8
debaser9 <jason@jasonbraswell.com>
2024-11-21 10:54:49 -1000

    add 2024 wobble gobble

--- src/data/races.js
+++ src/data/races.js
@@ -2,9 +2,9 @@ export const upcomingRaces = [
   // DO NOT DELETE THE FIRST ENTRY ("Please select...") OR MOVE IT!!!
   { name: "Please select a race" },
   {
-    name: "Test Race 1",
-    distances: ["4 mile", "5 mile", "6 mile"],
-    password: "1234",
+    name: "2024_Wobble_Gobble",
+    distances: ["1 mile", "3.5 mile", "6.4 mile"],
+    password: "2024",
   },
 ];
 
@@ -12,15 +12,15 @@ export const allRaces = [
   // DO NOT DELETE THE FIRST ENTRY ("Please select...") OR MOVE IT!!!
   { name: "Please select a race" },
   {
-    name: "2022 Wobble Gobble",
-    distances: ["1 mile", "3.5 miles", "6.4 miles"],
+    name: "2024_Wobble_Gobble",
+    distances: ["1 mile", "5k", "10k"],
   },
   {
     name: "2023_Wobble_Gobble",
     distances: ["1 mile", "3.5 miles", "6.4 miles"],
   },
   {
-    name: "Test Race 1",
-    distances: ["1 mile", "5k", "10k"],
+    name: "2022 Wobble Gobble",
+    distances: ["1 mile", "3.5 miles", "6.4 miles"],
   },
 ];
